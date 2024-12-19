38541ad829ff207c835c898db627bef1b38e2de1
SuraCdac <sura.cdac@gmail.com>
2024-12-18 17:16:24 -0800

    c# features partial classes

---
+++ Basics/Basics/28CSharpDemos/File1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28CSharpDemos
+{
+
+    public class AdvMath
+    {
+        public void sayHii()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+    public partial class CMath : AdvMath
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+    }
+}
---
+++ Basics/Basics/28CSharpDemos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _28CSharpDemos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CMath cMath = new CMath();
+            Console.WriteLine($"Add : {cMath.add(10, 20)}");
+            Console.WriteLine($"Add : {cMath.sub(10, 20)}");
+            cMath.sayHii();
+        }
+    }
+}
