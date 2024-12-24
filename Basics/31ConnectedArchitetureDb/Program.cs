853b5d0d91eb76c0d0d9c22d09aaa4c9c5a4d45c
SuraCdac <sura.cdac@gmail.com>
2024-12-23 16:58:16 -0800

    Database connection Disconnected Architechture

---
+++ Basics/31ConnectedArchitetureDb/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _31ConnectedArchitetureDb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region CRUD Application with Console.WriteLine
+            string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Database;Integrated Security=True;Pooling=False;Trust Server Certificate=False";
+
+            #region Select Query
+            SqlConnection con = new SqlConnection(conStr);
+            String selectQuery = "select * from Emp";
+            SqlCommand cmd = new SqlCommand(selectQuery, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) 
+            {
+                int id = Convert.ToInt32(reader["Id"]);
+                string? Name = Convert.ToString(reader["Name"]);
+                string? Address = Convert.ToString(reader["Address"]);
+                Console.WriteLine($"Id : {id}, Name :{Name}, Address:{Address}");
+            }
+            con.Close();
+
+            #endregion Insert Query
+
+
+
+            #region 
+
+            #endregion
+            #endregion
+        }
+    }
+}
---
+++ Basics/Database/32DIsconnectedArchitecture/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace _32DIsconnectedArchitecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Pooling=False;Trust Server Certificate=False";
+
+            #region Disconnected Architechture select query
+            SqlConnection con = new SqlConnection(conStr);
+            string selectQuery = "select * from Employee";
+            SqlDataAdapter da = new SqlDataAdapter(selectQuery, con);
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine($"ID : {dr["Id"]}, Name : {dr["Name"]}, Address : {dr["Address"]}");
+            //} 
+            #endregion
+
+
+            #region Disconnected Architechture insert query
+            //Console.WriteLine("Enter Name : ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter Address : ");
+            //string address = Console.ReadLine();
+            //DataRow row = dt.NewRow();
+            //row["Name"] = name;
+            //row["Address"] = address;
+            //dt.Rows.Add(row);
+            //da.Update(dt); 
+            #endregion
+
+
+            #region Disconnected Architechture update query
+            //Console.WriteLine("Enter ID : ");
+            //int id = Convert.ToInt32(Console.ReadLine());
+            //DataRow roempToBeEdited = dt.Rows.Find(id);
+            //if (roempToBeEdited != null)
+            //{
+            //    Console.WriteLine("Enter Name : ");
+            //    string name = Console.ReadLine();
+            //    Console.WriteLine("Enter Address : ");
+            //    string address = Console.ReadLine();
+            //    roempToBeEdited["Name"] = name;
+            //    roempToBeEdited["Address"] = address;
+            //    da.Update(dt);
+            //    Console.WriteLine("Employee details updated successfully.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee is not present in database.");
+            //} 
+            #endregion
+
+            #region Disconnected Architechture delete query
+            //Console.WriteLine("Enter ID : ");
+            //int id = Convert.ToInt32(Console.ReadLine());
+            //DataRow? empToBeDeleted = dt.Rows.Find(id);
+            //Console.WriteLine("Employee details deleted successfully.");
+            //if (empToBeDeleted != null )
+            //{
+            //    empToBeDeleted.Delete();
+            //    da.Update(dt);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employee is not present in database.");
+            //}
+            #endregion
+
+        }
+    }
+}
