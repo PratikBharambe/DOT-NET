7c7067f2f160a25cc8be7af7f02e155c28b723f9
SuraCdac <sura.cdac@gmail.com>
2024-12-27 06:08:05 +0530

    MVC 6

--- WebDemos/MVC^/04ActionMethodReturnType/obj/Debug/net6.0/04ActionMethodReturnType.AssemblyInfo.cs
+++ WebDemos/MVC^/04ActionMethodReturnType/obj/Debug/net6.0/04ActionMethodReturnType.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04ActionMethodReturnType")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69cd741401050bd741dd4066d8ec5bc0a9f36d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("04ActionMethodReturnType")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04ActionMethodReturnType")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
