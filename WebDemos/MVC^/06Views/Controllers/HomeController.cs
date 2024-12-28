e30657023b75c2172ebed7ffb8533f9154ce81ba
SuraCdac <sura.cdac@gmail.com>
2024-12-27 17:06:40 -0800

    MVC Views

---
+++ WebDemos/MVC^/06Views/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using _06Views.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _06Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Emp emp = new Emp() { Id = 15, Name = "Pratik", 
+            Address = "Pune"};
+            Cust cust = new Cust() { Id = 41, Name = "Lalit", Address = "Nanded" };
+            ArrayList arrList = new ArrayList() { emp, cust };
+            return View(arrList);
+        }
+    }
+}
---
+++ WebDemos/MVC^/06Views/Models/Emp.cs
@@ -0,0 +1,11 @@
+﻿namespace _06Views.Models
+{
+    public class Emp
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+
+    }
+}
