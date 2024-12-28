fb413a6723c456b4fda6e2eb6386b0cd7ede410e
SuraCdac <sura.cdac@gmail.com>
2024-12-27 17:07:12 -0800

    MVC ModelViews

---
+++ WebDemos/MVC^/07ViewModel/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using _07ViewModel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _07ViewModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductViewModel product = new ProductViewModel()
+            {
+                Product = new Product() { Id = 404, Name = "ASUS Laptop", Price = 75800},
+                CatagoryName = "Electronics",
+                InStock = false,
+                Message = "Company : ASUS, i7, Gen 11, RAM : 32GB"
+            };
+            return View(product);
+        }
+
+        public IActionResult ProductDetails(int id)
+        {
+            Product product = new Product() { 
+                Id = 404,
+                Name = "ASUS Laptop",
+                Price = 75000
+            };
+            return View(product);
+        }
+    }
+}
---
+++ WebDemos/MVC^/07ViewModel/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace _07ViewModel.Models
+{
+    public class ProductViewModel
+    {
+        public Product? Product { get; set; }
+        public string? CatagoryName { get; set; }
+        public bool InStock { get; set; }
+        public string? Message { get; set; }
+    }
+}
