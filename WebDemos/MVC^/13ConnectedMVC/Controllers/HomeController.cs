9fd75d79c98e5bac1afeeb662411c8b9219b234d
SuraCdac <sura.cdac@gmail.com>
2024-12-28 16:36:45 -0800

    MVC Connected database

---
+++ WebDemos/MVC^/13ConnectedMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using _13ConnectedMVC.DAL;
+using _13ConnectedMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _13ConnectedMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            TestDbContext testDbContext = new TestDbContext();
+            var allEmps = testDbContext.GetAllEMployees();
+            return View(allEmps);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebDemos/MVC^/13ConnectedMVC/DAL/TestDbContext.cs
@@ -0,0 +1,30 @@
+﻿using _13ConnectedMVC.Models;
+using Microsoft.Data.SqlClient;
+
+namespace _13ConnectedMVC.DAL
+{
+    public class TestDbContext
+    {
+        string constr = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True";
+
+        public List<Emp> GetAllEMployees()
+        {
+            List<Emp> empList = new List<Emp>();
+            SqlConnection con = new SqlConnection(constr);
+            string selectQuery = "select * from Employee";
+            SqlCommand cmd = new SqlCommand(selectQuery, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Emp emp = new Emp();
+                emp.Id = Convert.ToInt32(reader["id"]);
+                emp.Name = Convert.ToString(reader["Name"]);
+                emp.Address = Convert.ToString(reader["Address"]);
+                empList.Add(emp);
+            }
+            return empList;
+        }
+
+    }
+}
