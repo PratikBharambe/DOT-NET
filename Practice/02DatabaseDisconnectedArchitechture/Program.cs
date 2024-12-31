c33800eb2a52412d69cb9d816e5a90da4b84222e
SuraCdac <sura.cdac@gmail.com>
2024-12-31 21:48:29 +0530

    pratice discooonected architechture

---
+++ Practice/02DatabaseDisconnectedArchitechture/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _02DatabaseDisconnectedArchitechture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string constr = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=AudioBook;Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(constr);
+            string selecrQuery = "select * from Books";
+            SqlDataAdapter da = new SqlDataAdapter();
+
+        }
+    }
+}
