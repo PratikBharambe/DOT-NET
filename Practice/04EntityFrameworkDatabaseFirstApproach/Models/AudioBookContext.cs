29afcc4414ffbd37ba52e73768c775cf14690898
SuraCdac <sura.cdac@gmail.com>
2025-01-01 11:00:35 +0530

    Entity framework database first approach

---
+++ Practice/04EntityFrameworkDatabaseFirstApproach/Models/AudioBookContext.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace _04EntityFrameworkDatabaseFirstApproach.Models
+{
+    public partial class AudioBookContext : DbContext
+    {
+        public AudioBookContext()
+        {
+        }
+
+        public AudioBookContext(DbContextOptions<AudioBookContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=AudioBook;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.Property(e => e.Author)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Narrator)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Practice/04EntityFrameworkDatabaseFirstApproach/Program.cs
@@ -0,0 +1,91 @@
+﻿using _04EntityFrameworkDatabaseFirstApproach.Models;
+
+namespace _04EntityFrameworkDatabaseFirstApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+            AudioBookContext dbContest = new AudioBookContext();
+
+            do
+            {
+                Console.WriteLine("1. Get all Users.");
+                Console.WriteLine("2. Add new user.");
+                Console.WriteLine("3. Update user details.");
+                Console.WriteLine("4. Delete user.");
+                Console.WriteLine("5. Exit.");
+                Console.WriteLine("Enter your choice : .");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        List<User> users = dbContest.Users.ToList();
+                        if(users != null)
+                            users.ForEach(u => Console.WriteLine($"Id : {u.Id}, Username : {u.Username}, Password : {u.Password}"));
+                        break;
+
+                    case 2:
+                        User user = new User();
+                        Console.WriteLine("Enter Username : ");
+                        user.Username = Console.ReadLine();
+                        Console.WriteLine("Enter Password : ");
+                        user.Password = Console.ReadLine();
+                        dbContest.Users.Add(user);
+                        dbContest.SaveChanges();
+                        Console.WriteLine("User added successfully.");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Enter id : ");
+                        int id = Convert.ToInt32(Console.ReadLine());
+
+                        user = dbContest.Users.Find(id);
+                        if(user != null)
+                        {
+                            Console.WriteLine("Enter Username : ");
+                            user.Username = Console.ReadLine();
+                            Console.WriteLine("Enter Password : ");
+                            user.Password = Console.ReadLine();
+                            dbContest.SaveChanges();
+                            Console.WriteLine("User information updated.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error occured.");
+                        }
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter id : ");
+                        id = Convert.ToInt32(Console.ReadLine());
+
+                        user = dbContest.Users.Find(id);
+                        if(user != null)
+                        {
+                            dbContest.Users.Remove(user);
+                            dbContest.SaveChanges();
+                            Console.WriteLine("User deleted successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error occured.");
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Thanks for visiting !!!\nBye Bye .........");
+                        break;
+
+                    default:
+                        Console.WriteLine("invalid choice.");
+                        break;
+                }
+            }
+            while (choice != 5);
+        }
+    }
+}
